8abd2a83cedd44fa7a9c9d4bb6845a6cbc981d84
LUISZC\Luis <luiscarcelen7@gmail.com>
2016-10-20 17:45:17 -0500

    Vista de Imagenes

--- controllers/ImgController.js
+++ controllers/ImgController.js
@@ -71,7 +71,7 @@ module.exports = {
          var base64Image = original_data.toString('base64');
          //console.log(base64Image);
          session
-        .run('CREATE(img:Image {vista:{vistaParam}, area:{areaParam}, base64:{base64Param}, tecnica:{tecnicaParam}, procedimiento:{procedimientoParam}, enfoque:{enfoqueParam}, observaciones:{observacionesParam}})',{vistaParam:vista, areaParam:area, base64Param:base64Image,tecnicaParam:tecnica, procedimientoParam:procedimiento, enfoqueParam:enfoque, observacionesParam:observaciones})
+        .run('CREATE(img:Image {view:{vistaParam}, area:{areaParam}, base64:{base64Param}, technique:{tecnicaParam}, process:{procedimientoParam}, focus:{enfoqueParam}, observations:{observacionesParam}})',{vistaParam:vista, areaParam:area, base64Param:base64Image,tecnicaParam:tecnica, procedimientoParam:procedimiento, enfoqueParam:enfoque, observacionesParam:observaciones})
          //   .run('CREATE(img:Image {base64:{base64Param}, type:{typeParam}})',{base64Param: base64Image,typeParam: tipo})
       
          .then(function(result){
@@ -163,21 +163,38 @@ module.exports = {
            
    },
    postViewImg: function(req, res, next){
-    var area = req.body.selectpickerArea;
-    console.log(area);
+    var paciente = req.body.selectpickerPaciente;
+    //console.log(area);
     var session = require('.././database/config');
     session
-    .run('MATCH(img:Image) WHERE img.area={areaParam} RETURN img.base64',{areaParam: area})
+   // .run('MATCH(img:Image) WHERE img.area={areaParam} RETURN img.base64',{areaParam: area})
+  //  .run('MATCH(img:Image)-[blg:belongs]-> (ptn:Patient) WHERE ptn.name={pacienteParam} RETURN img, ptn', {pacienteParam: paciente})
+    .run('MATCH(dr:Doctor)-[rsp:responsable]-> (img:Image)-[blg:belongs]-> (ptn:Patient)WHERE ptn.name={pacienteParam} RETURN img, ptn, dr', {pacienteParam: paciente})
     .then(function(result){
-      var resultado;
+      var imgArr = [];
+      
       result.records.forEach(function(record){
-         resultado= record._fields[0]
-      });
+         imgArr.push({
+          id: record._fields[0].identity.low,
+          vista: record._fields[0].properties.view,
+          area: record._fields[0].properties.area,
+          img: record._fields[0].properties.base64,
+          tecnica: record._fields[0].properties.technique,
+          proceso: record._fields[0].properties.process,
+          enfoque: record._fields[0].properties.focus,
+          observaciones: record._fields[0].properties.observations,
+          name: record._fields[1].properties.name,
+          genero: record._fields[1].properties.gender,
+          namedoctor: record._fields[2].properties.name
+           
+         });
+        });
+        console.log(imgArr);
       res.render('users/viewImg2',{
-        imgArea: area, 
-        resultado: resultado
+        imgPaciente: paciente, 
+        images: imgArr
     });
-       console.log(resultado);
+      // console.log(resultado);
     })
     .catch(function(err){
       console.log(err);
